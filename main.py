39a2fcae713d206eec3ea87dff7ca645bf5108f1
dogogh <quadros1919@gmail.com>
2024-04-02 19:05:10 -0300

    excel

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
+lista=[] 
 url = 'https://g1.globo.com'
-
 
 response = requests.get(url)
 content = response.content
 
-
 site = BeautifulSoup(content, 'html.parser')
 
+noticias = site.findAll('div', {'class': 'feed-post-body'})
 
-noticia = site.find('div',{'class': 'feed-post-body'}) 
+for noticia in noticias:
+    titulo = noticia.find('p', {'elementtiming': 'text-ssr'})
 
-titulo=noticia.find('a',{'class','feed-post-figure-link'})
-foto= titulo.find('img')
+    resumo_div= noticia.find('div', {'class': 'feed-post-body-resumo'})
+    if(resumo_div):
+        resumo=resumo_div.find('p', {'elementtiming': 'text-ssr'})
+        lista.append([titulo.text,resumo.text])
+    else:
+        lista.append([titulo.text])
 
-print(foto.prettify())
-
-
-
-
+news = pd.DataFrame(lista, columns=['Titulo','Resumo'])
+news.to_excel('estudos.xlsx', engine='openpyxl', index=False)
