8d4373bdd3a6b52bbc0646956250eba263c6d30a
dogogh <quadros1919@gmail.com>
2024-04-09 11:42:19 -0300

    antes de next

--- eBook.py
+++ eBook.py
@@ -3,18 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
-import os
 
-# Opções do Firefox
-options = webdriver.FirefoxOptions()
+options = webdriver.SafariOptions()
 options.headless = True
-
-# Path do GeckoDriver
-gecko_driver_path = '/path/to/geckodriver'  # Substitua pelo caminho do seu GeckoDriver
-os.environ['PATH'] += ':' + os.path.dirname(gecko_driver_path)
-
-# Inicializando o driver do Firefox
-driver = webdriver.Firefox(options=options)
+driver = webdriver.Safari(options=options)
 
 url_base = 'https://www.amazon.com/'
 main_route = 's?i=stripbooks&rh=n%3A283155%2Cp_30%3AForgotten+Books%2Cp_n_feature_browse-bin%3A2656022011&s=exact-aware-popularity-rank&dc&language=pt&ds=v1%3AF62MM2U5pQxy5ihMMkiYz3DXl5SKe%2FaUTy9uSUSDxkc&Adv-Srch-Books-Submit.x=35&Adv-Srch-Books-Submit.y=4&qid=1712363856&unfiltered=1&ref=sr_st_exact-aware-popularity-rank'
@@ -23,57 +15,41 @@ sleep(2)
 
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 's-result-item')))
 
-# Função para reavaliar a lista de elementos
-def get_name_elements():
-    return driver.find_elements(By.XPATH, "//span[@class='a-size-medium a-color-base a-text-normal']")
+name_element = driver.find_element(By.XPATH, "//span[@class='a-size-medium a-color-base a-text-normal']")
 
-name_elements = get_name_elements()
 
-for i in range(len(name_elements)):
-    name_elements = get_name_elements()  # Reavaliar a lista de elementos
-    name_element = name_elements[i]
-    name_element.click()
-    sleep(5)
 
-    try:
-        name_detail = driver.find_element(By.XPATH, "//span[@class='a-size-medium a-color-base a-text-normal']")
-    except:
-        print("Elemento name_detail não encontrado.")
-        continue  # Vamos para a próxima iteração
+name_element.click() 
+sleep(5)
 
-    price_detail = driver.find_element(By.XPATH, "//span[@class='a-size-medium a-color-price header-price a-text-normal']")
-    detail_section = driver.find_element(By.ID, 'detailBulletsWrapper_feature_div')
+name_detail = driver.find_element(By.XPATH, "//span[@class='a-size-extra-large celwidget']")
+price_detail = driver.find_element(By.XPATH, "//span[@class='a-size-medium a-color-price header-price a-text-normal']")
+detail_section = driver.find_element(By.ID, 'detailBulletsWrapper_feature_div')
 
-    uls = detail_section.find_elements(By.CLASS_NAME, 'a-unordered-list')
+uls = detail_section.find_elements(By.CLASS_NAME, 'a-unordered-list')
 
-    if len(uls) >= 2:
-        first_ul = uls[0]
-        firstList_ul = first_ul.find_elements(By.TAG_NAME, 'li')
-        second_ul = uls[1]
-        secondList_ul = second_ul.find_elements(By.TAG_NAME, 'li')
+if len(uls) >= 2:
+    first_ul = uls[0]
+    firstList_ul = first_ul.find_elements(By.TAG_NAME, 'li')
+    second_ul = uls[1]
+    secondList_ul = second_ul.find_elements(By.TAG_NAME, 'li')
 
-        print("Book:", name_detail.text, price_detail.text)
+print("Book:", name_detail.text, price_detail.text)
 
-        for i in range(1, len(firstList_ul)):
-            spans = firstList_ul[i].find_elements(By.TAG_NAME, 'span')
-            if len(spans) >= 2:
-                print(spans[2].text.strip(), end=', ')
+for i in range(1, len(firstList_ul)): 
+    spans = firstList_ul[i].find_elements(By.TAG_NAME, 'span')
+    if len(spans) >= 2:
+        print(spans[2].text.strip(), end=', ')
 
-        # Print information from the second ul
-        for li in secondList_ul:
-            spans = li.find_elements(By.TAG_NAME, 'span')
-            if spans:
-                for index, span in enumerate(spans):
-                    print(f"[{index}] {span.text.strip()}", end=', ')
-            else:
-                print("Two uls with the class 'a-unordered-list' were not found")
-
-        # Voltar para a página anterior
-        try:
-            driver.execute_script("window.history.go(-1)")
-        except:
-            print("Failed to go back")
-
-        sleep(5)
-
+# Print information from the second ul
+for li in secondList_ul:
+    spans = li.find_elements(By.TAG_NAME, 'span')
+    if spans:
+        for index, span in enumerate(spans):
+                print(f"[{index}] {span.text.strip()}", end=', ')
+else:
+    print("Two uls with the class 'a-unordered-list' were not found")
+    
+driver.back()
+sleep(5)
 driver.quit()
--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
-lista=[] 
-url = 'https://g1.globo.com'
+options = webdriver.SafariOptions()
+options.headless = True
+driver = webdriver.Safari(options=options)
 
-response = requests.get(url)
-content = response.content
+url_base = 'https://www.amazon.com/'
+main_route = 's?i=stripbooks&rh=n%3A283155%2Cp_30%3AForgotten+Books%2Cp_n_feature_browse-bin%3A2656022011&s=exact-aware-popularity-rank&dc&language=pt&ds=v1%3AF62MM2U5pQxy5ihMMkiYz3DXl5SKe%2FaUTy9uSUSDxkc&Adv-Srch-Books-Submit.x=35&Adv-Srch-Books-Submit.y=4&qid=1712363856&unfiltered=1&ref=sr_st_exact-aware-popularity-rank'
+driver.get(url_base + main_route)
+sleep(2)
 
-site = BeautifulSoup(content, 'html.parser')
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 's-result-item')))
 
-noticias = site.findAll('div', {'class': 'feed-post-body'})
+name_elements = driver.find_elements(By.XPATH, "//span[@class='a-size-medium a-color-base a-text-normal']")
 
-for noticia in noticias:
-    titulo = noticia.find('p', {'elementtiming': 'text-ssr'})
+index = 1
+for name_element in name_elements:
+    name_element.click()
+    sleep(2)
+    print(f"Anúncio {index}")
+    driver.back()
+    sleep(2)
+    index += 1
 
-    resumo_div= noticia.find('div', {'class': 'feed-post-body-resumo'})
-    if(resumo_div):
-        resumo=resumo_div.find('p', {'elementtiming': 'text-ssr'})
-        lista.append([titulo.text,resumo.text])
-    else:
-        lista.append([titulo.text])
-
-news = pd.DataFrame(lista, columns=['Titulo','Resumo'])
-news.to_excel('estudos.xlsx', engine='openpyxl', index=False)
+driver.quit()
