8a3ce2410869169cad188304e6941ce0e162cfc8
dogogh <quadros1919@gmail.com>
2024-04-09 19:48:56 -0300

    nao funciona next

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import os
+
 os.system('clear')
+
 options = webdriver.SafariOptions()
 options.headless = True
 driver = webdriver.Safari(options=options)
@@ -14,24 +16,28 @@ main_route = 's?i=stripbooks&rh=n%3A283155%2Cp_30%3AForgotten+Books%2Cp_n_featur
 driver.get(url_base + main_route)
 sleep(2)
 
-#WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 's-result-item')))
 pagination = driver.find_element(By.XPATH,"//span[@class='s-pagination-item s-pagination-disabled']")
-num=int(pagination.text)
-print(num-1)
-for page in range(num-1):   
-
+num = int(pagination.text)
+print(num - 1)
+for page in range(num - 1):   
     name_elements = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//span[@class='a-size-medium a-color-base a-text-normal']")))
-
     index = 1
     for name_element in name_elements:
+        print(f"Anúncio {index}")
+        print(name_element.text)  
         name_element.click()
         WebDriverWait(driver, 10).until(EC.url_changes)  
-        print(f"Anúncio {index}")
+        
         driver.back()
         WebDriverWait(driver, 10).until(EC.url_changes)  
         index += 1
-    
-    next_element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//a[@class='s-pagination-item s-pagination-next s-pagination-button s-pagination-separator']")))
-    next_element.click()
-    print("Page:", page + 2)
+    sleep(2)
+    try:
+        next_button = driver.find_element(By.XPATH, "//a[@aria-label='Ir para a próxima página, página 2']")
+        next_button.click()
+        sleep(15)  # Aumentando o tempo de espera para garantir que a próxima página seja carregada completamente
+        print("Page:", page + 2)    
+    except:
+        print("O botão 'Next' não foi encontrado ou não é visível.")
+        break
 driver.quit()
