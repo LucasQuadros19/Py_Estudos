7aaa9648217eacd03b3ddb9511559640225079e2
dogogh <quadros1919@gmail.com>
2024-04-10 19:26:09 -0300

    next erro

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import os
 
 os.system('clear')
 
-options = webdriver.SafariOptions()
+options = webdriver.FirefoxOptions()
 options.headless = True
-driver = webdriver.Safari(options=options)
+driver = webdriver.Firefox(options=options)
 
 url_base = 'https://www.amazon.com/'
 main_route = 's?i=stripbooks&rh=n%3A283155%2Cp_30%3AForgotten+Books%2Cp_n_feature_browse-bin%3A2656022011&s=exact-aware-popularity-rank&dc&language=pt&ds=v1%3AF62MM2U5pQxy5ihMMkiYz3DXl5SKe%2FaUTy9uSUSDxkc&Adv-Srch-Books-Submit.x=35&Adv-Srch-Books-Submit.y=4&qid=1712363856&unfiltered=1&ref=sr_st_exact-aware-popularity-rank'
@@ -19,25 +19,36 @@ sleep(2)
 pagination = driver.find_element(By.XPATH,"//span[@class='s-pagination-item s-pagination-disabled']")
 num = int(pagination.text)
 print(num - 1)
-for page in range(num - 1):   
+
+for page in range(num - 1):
+    print("URL atual:", driver.current_url)
     name_elements = WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//span[@class='a-size-medium a-color-base a-text-normal']")))
     index = 1
+    print("URL atual:", driver.current_url)
     for name_element in name_elements:
-        print(f"Anúncio {index}")
-        print(name_element.text)  
-        name_element.click()
-        WebDriverWait(driver, 10).until(EC.url_changes)  
-        
-        driver.back()
-        WebDriverWait(driver, 10).until(EC.url_changes)  
-        index += 1
+        try:
+            print(name_element.text)  
+            name_element.click()
+            WebDriverWait(driver, 10).until(EC.url_changes)  
+            print(f"Anúncio {index}")
+            driver.back()
+            WebDriverWait(driver, 10).until(EC.url_changes)  
+            index += 1
+        except Exception as e:
+            print(f"Erro ao interagir com o elemento: {e}")
+            continue
     sleep(2)
+
     try:
-        next_button = driver.find_element(By.XPATH, "//a[@aria-label='Ir para a próxima página, página 2']")
+        print("URL atual:", driver.current_url)
+        next_button = driver.find_element(By.XPATH, "//a[contains(@class, 's-pagination-item') and contains(text(), 'Próximo')]")
         next_button.click()
-        sleep(15)  # Aumentando o tempo de espera para garantir que a próxima página seja carregada completamente
-        print("Page:", page + 2)    
+        print("Página:", page + 2)
+        WebDriverWait(driver, 10).until(EC.url_changes)  
+        print("URL atual:", driver.current_url)
+        sleep(10)
     except:
         print("O botão 'Next' não foi encontrado ou não é visível.")
         break
+
 driver.quit()
